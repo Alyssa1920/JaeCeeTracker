35fbfb6a87a49324cc5f5f6f5395e421bde20bc6
Alyssa1920 <74675618+Alyssa1920@users.noreply.github.com>
2021-04-08 23:55:49 -0400

    Add files via upload

---
+++ JaeCeeTracker/src/main/java/com/jae/cee/JaeCeeTracker/JaeCeeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jae.cee.JaeCeeTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaeCeeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaeCeeTrackerApplication.class, args);
+	}
+
+}
---
+++ JaeCeeTracker/src/test/java/com/jae/cee/JaeCeeTracker/JaeCeeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jae.cee.JaeCeeTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaeCeeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
